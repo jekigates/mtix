5ffa589b01b893e90ea5c304777609c67f875301
jekigates2004 <jeki.gates@binus.ac.id>
2024-06-13 12:09:17 +0700

    Try to fix https over http part 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(App::environment('production'))
-        {
+        if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
         }
 
